925807e865e87fa05a557d5a6d92123c8fcd44fd
Adam Pridmore <adam_pridmore@hotmail.com>
2016-05-09 20:47:09 +0100

    - Tweaks  - Added unit test

---
+++ src/test/java/ProjectEulerJava/Problem2Test.java
@@ -0,0 +1,15 @@
+package ProjectEulerJava;
+
+import org.junit.Test;
+
+import static ProjectEulerJava.Problem2.solver;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+public class Problem2Test {
+    @Test
+    public void test() {
+        assertThat(solver(), is(231L));
+    }
+}
